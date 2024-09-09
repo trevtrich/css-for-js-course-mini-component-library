4dd88ad485add3554b9674149bb67aeb19f660bb
Trevor Richardson <trevor@gaincompliance.com>
2024-09-09 15:49:25 -0500

    icon input complete

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,11 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZE = {
+    small: {height: 24, imageSize: 16, fontSize: 14, borderThickness: 1},
+    large: {height: 36, imageSize: 24, fontSize: 18, borderThickness: 2},
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +18,39 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <Wrapper style={{'--borderThickness': `${SIZE[size].borderThickness}px`, '--width': `${width}px`}}>
+      <form>
+          <Icon id={icon} size={SIZE[size].imageSize} style={{display: 'inline-block', paddingRight: '0.5', position: 'absolute', top: 0, bottom: 0, margin: 'auto', '--size': `${SIZE[size].imageSize}px`}}/>
+          <VisuallyHidden><Label htmlFor="text-input">Would you like to enter some text?</Label></VisuallyHidden>
+          <Input id="text-input" type="text" placeholder={placeholder} style={{'--height': `${SIZE[size].height}px`, '--imageSpacer': `${SIZE[size].imageSize + 8}px`, '--fontSize': `${SIZE[size].fontSize}px`}} />
+      </form>
+  </Wrapper>;
 };
+
+const Input = styled.input`
+    width: 100%;
+    height: var(--height);
+    padding-left: var(--imageSpacer);
+    font-size: var(--fontSize);
+    font-weight: 700;
+    border: none;
+    ::placeholder {
+        font-weight: 400;
+    }
+    color: ${COLORS.gray700};
+    :hover {
+        color: ${COLORS.black};
+    }
+`;
+
+const Label = styled.label`
+`;
+
+const Wrapper = styled.div`
+    width: var(--width);
+    position: relative;
+    border-bottom: solid ${COLORS.black};
+    border-bottom-width: var(--borderThickness);
+`;
 
 export default IconInput;
