2369320a5161d959315dedc72df9d9b929e5b3e2
Trevor Richardson <trevor@gaincompliance.com>
2024-09-09 09:51:13 -0500

    progress bar complete

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,8 +6,40 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
+  if (size === 'large') {
+    return <div style={{
+        height: 24,
+        borderRadius: 8,
+        overflow: 'hidden',
+        padding: '4px',
+        backgroundColor: 'hsla(0, 0%, 50%, 0.15)',
+        boxShadow: 'inset 0 2px 4px hsla(0, 0%, 50%, 0.35)'
+    }}>
+        <div style={{
+            backgroundColor: 'hsla(0, 0%, 50%, 0.15)',
+            borderRadius: '4px',
+            overflow: 'hidden',
+            width: '100%',
+            height: '100%'
+        }}>
+            <div style={{
+                backgroundColor: 'hsla(240, 80%, 60%, 1)',
+                width: `${value}%`,
+                height: '100%'
+            }} />
+        </div>
+    </div>;
+  } else if (size === 'medium') {
+      return <div style={{
+          height: 12,
+          borderRadius: 4,
+          overflow: 'hidden',
+          backgroundColor: 'hsla(0, 0%, 50%, 0.15)', boxShadow: 'inset 0 2px 4px hsla(0, 0%, 50%, 0.35)'}}>
+      <div style={{backgroundColor: 'hsla(240, 80%, 60%, 1)', width: `${value}%`, height: '100%'}} />
+    </div>;
+  }
 
-  return <div style={{height: 24, borderRadius: 8, overflow: 'hidden', border: '4px solid hsla(0, 0%, 50%, 0.15)'}}>
+  return <div style={{height: 8, borderRadius: 4, overflow: 'hidden', backgroundColor: 'hsla(0, 0%, 50%, 0.15)', boxShadow: 'inset 0 2px 4px hsla(0, 0%, 50%, 0.35)'}}>
     <div style={{backgroundColor: 'hsla(240, 80%, 60%, 1)', width: `${value}%`, height: '100%'}} />
   </div>;
 };
